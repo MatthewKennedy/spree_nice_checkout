b2c156833b67264ee21fbf43d933e238b8f6bc39
Matthew Kennedy <m.kennedy@me.com>
2019-07-28 10:06:51 +0100

    add email form validation

--- app/assets/javascripts/spree/frontend/checkout/address.js
+++ app/assets/javascripts/spree/frontend/checkout/address.js
@@ -104,4 +104,25 @@ Spree.ready(function ($) {
     }
   }
   Spree.onAddress()
+
+  function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
+  function validate() {
+    var $result = $("#result");
+    var email = $("#email").val();
+    $result.text("");
+
+    if (validateEmail(email)) {
+    } else {
+      $result.text(email + " is not a valid email address");
+      $result.css("color", "red");
+      return false;
+    }
+
+  }
+  $("#address-form-button").on("click", validate);
+
 })
