b321124a0eb9ba8af562a41e2c4f8d74b446b36c
Gonzalo Moreno <goncab380@hotmail.com>
2019-07-31 00:37:36 -0400

    added geocoder and country lookup by request

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -2,15 +2,12 @@ module Spree
   CheckoutController.class_eval do
     before_action :update_marketing_preferences, only: :update
 
-    def update_marketing_preferences
-      return unless params[:state] == "address"
-      @user ||= try_spree_current_user
-      @user.update_attributes(email_marketing) if @user
-    end
-
-    def summary_adjustments
-      render partial: "spree/checkout/summary_adjustments", locals: { order: simple_current_order }
-      fresh_when(simple_current_order)
+    def before_address
+      default_country = Spree::Country.find_by(iso: country_iso_lookup) if country_iso_lookup.present?
+      # if the user has a default address, a callback takes care of setting
+      # that; but if he doesn't, we need to build an empty one here
+      @order.bill_address ||= default_country ? Address.new(country: default_country) : Address.build_default
+      @order.ship_address ||= default_country ? Address.new(country: default_country) : Address.build_default if @order.checkout_steps.include?("delivery")
     end
 
     def remove_coupon
@@ -23,7 +20,24 @@ module Spree
       end
     end
 
+    def summary_adjustments
+      render partial: "spree/checkout/summary_adjustments", locals: { order: simple_current_order }
+      fresh_when(simple_current_order)
+    end
+
+    def update_marketing_preferences
+      return unless params[:state] == "address"
+      @user ||= try_spree_current_user
+      @user.update_attributes(email_marketing) if @user
+    end
+
     private
+
+    def country_iso_lookup
+      return request.headers["CF-IPCountry"] if request.headers["CF-IPCountry"].present?
+
+      request.location.country
+    end
 
     def email_marketing
       params.require(:user).permit(:accepts_marketing_emails)
