9c3773414c633f8e518d9815626b286728666319
Matthew Kennedy <m.kennedy@me.com>
2019-08-01 11:49:51 +0100

    add

--- app/assets/javascripts/spree/frontend/checkout/shipment.js
+++ app/assets/javascripts/spree/frontend/checkout/shipment.js
@@ -50,6 +50,7 @@ var activeItem = $('#shipping_methods').find(':checked')
 activeItem.closest('li').addClass('active_shipping_method')
 
 $("#shipping_methods li label").on('click', function(){
+  
   $('li').removeClass("active_shipping_method");
   $(this).closest('li').toggleClass("active_shipping_method");
 });
